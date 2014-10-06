a72111b07156343a0ea1511dd159eb770eeeaf52
Karthik Saligrama <coolspark@gmail.com>
2014-10-06 14:49:10 -0700

    Initial Commit

---
+++ MQTTExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MQTTExample
+//
+//  Created by Karthik Saligrama on 10/6/14.
+//  Copyright (c) 2014 Karthik Saligrama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MQTTExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MQTTExample
+//
+//  Created by Karthik Saligrama on 10/6/14.
+//  Copyright (c) 2014 Karthik Saligrama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
